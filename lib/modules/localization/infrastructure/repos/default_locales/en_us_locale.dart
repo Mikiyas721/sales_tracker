0a437ef64e7e2f8e9ce0e481dd1de2130ad82007
Mikiyas721 <Mikiyas721@gmail.com>
2021-03-27 11:05:35 -0600

    Previous commit

--- lib/modules/localization/infrastructure/repos/default_locales/en_us_locale.dart
+++ lib/modules/localization/infrastructure/repos/default_locales/en_us_locale.dart
@@ -11,5 +11,83 @@ FLocale get enUsLocale {
 }
 
 const _enUsTranslationMap = {
-  //todo english translations go here
+  'splashPage': {'salesTracker': 'Sales Tracker'},
+  'loginPage': {
+    'welcome': 'Welcome',
+    'phoneNumber': 'Phone Number',
+    'submit': 'Submit',
+    'verify': 'Verify',
+    'codeSentMessage': 'We have sent a 6 digit verification code.\nPlease enter the code to continue.',
+    'verificationCode': 'verification code',
+    'wrongNumber': 'Wrong number?',
+    'noUserFound': 'No user found'
+  },
+  'homePage': {
+    'today': 'Today',
+    'thisWeek': 'This Week',
+    'thisMonth': 'This Month',
+    'sold': 'Sold',
+    'funded': 'Funded',
+    'loaned': 'Loaned',
+    'emptyMessage': 'You have no transaction',
+    'addShop': 'Add Shop',
+    'myShops': 'My Shops',
+    'signingOut': 'Signing out',
+    'logoutQuestion': 'Are you sure you want to log out?',
+    'cancel': 'Cancel',
+    'ok': 'Ok',
+    'undefinedSalesperson': 'Undefined Salesperson'
+  },
+  'newShopPage': {
+    'newShop': 'New Shop',
+    'prompt': 'Enter\nshop\ninformation',
+    'name': 'name',
+    'address': 'address',
+    'phoneNumber': 'phone number',
+    'add': 'Add',
+    'invalidInput': 'Invalid Input',
+    'undefinedSalesperson': 'Undefined Salesperson',
+    'successfulMessage': 'Operation Successful'
+  },
+  'myShopsPage': {
+    'searchPlaceHolder': 'Look for a shop',
+    'name': 'Name',
+    'phone': 'Phone',
+    'emptyShopsMessage': 'You have no shops. Click the button below to start registering',
+    'register': 'Register',
+    'sale': 'Sale',
+    'fund': 'Fund',
+    'card': 'Card',
+    'cash': 'Cash',
+    'undefinedSalesperson': 'Undefined Salesperson',
+  },
+  'sellingPage': {
+    'sale': 'Sale',
+    'prompt': 'Enter\ndetails',
+    'transactionInfo': 'Transaction with ',
+    'totalAmount': 'total amount',
+    'paidAmount': 'paid amount',
+    'register': 'Register',
+    'invalidInput': 'Invalid Input',
+    'undefinedSalesperson': 'Undefined Salesperson',
+    'successfulMessage': 'Successfully Added Sale'
+  },
+  'fundingPage': {
+    'fund': 'Fund',
+    'prompt': 'Enter\namount',
+    'transactionInfo': 'Funding for ',
+    'amount': 'amount',
+    'buttonLabel': 'Fund',
+    'invalidInput': 'Invalid Input',
+    'undefinedSalesperson': 'Undefined Salesperson',
+    'successfulMessage': 'Transaction Added Successfully'
+  },
+  'cardTransactionsPage': {
+    'cardTransactions': 'Card Transactions',
+    'undefinedSalesperson': 'Undefined Salesperson',
+  },
+  'cashTransactionsPage': {
+    'cashTransactions': 'Cash Transactions',
+    'undefinedSalesperson': 'Undefined Salesperson',
+  }
 };
